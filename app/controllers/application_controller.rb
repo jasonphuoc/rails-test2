b90761bd0681248ba6d5f4d59affe2fefeb0a263
Jason La <jasonphuoc@yahoo.com>
2017-07-18 09:55:31 -0700

    change route to temp msg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+    
+    def temp_page
+        render html: "page under construction"
+    end
 end
